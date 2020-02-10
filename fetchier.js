6807fe4dd92b78624c87a8417ceb134603259c3c
Mike Van <miike.vaan@gmail.com>
2020-02-10 18:38:36 +0700

    added headers to ws open

--- fetchier.js
+++ fetchier.js
@@ -31,144 +31,145 @@ const WSS_PROTOCOL = 'graphql-ws';
 const WSS_PROTOCOL_OLD = 'graphql-subscriptions';
 
 async function GET({ url, body, method = 'GET', headers = {}, token, debug }){
-  
-  if(!url) 
+
+  if(!url)
     throw new Error('url is missing');
-    
+
   try{
-    const res = await fetch(url, { 
-      method, 
-      body: JSON.stringify(body), 
+    const res = await fetch(url, {
+      method,
+      body: JSON.stringify(body),
       headers: {
         ...(token && {'authorization':  `Bearer ${token}`} || {}),
         ...headers
       }
     });
     const json = await res.json();
-    
+
     if(res && res.status !== 200)
       throw json;
-    
+
     debug && console.log('Fetchier GET:', { json });
     return json;
   } catch(error){ throw error }
 }
 
 async function POST({ url, body = {}, nocors, contentTypeForm, jsonBody = true, token, headers = {}, debug }){
-  
-  if(!url) 
+
+  if(!url)
     throw new Error('url is missing');
-  
+
   const opts = {
     method: 'POST',
     mode: nocors ? 'no-cors' : 'cors',
-    headers: { 
+    headers: {
       'content-type': !contentTypeForm ? 'application/json' : 'application/x-www-form-urlencoded',
       ...(token && {'authorization':  `Bearer ${token}`} || {}),
       ...headers
     },
     body: jsonBody ? JSON.stringify(body) : body
   }
-  
+
   try{
     const res = await fetch(url, opts);
     const json = await res.json();
-    
+
     if(res && res.status !== 200)
       throw json;
-      
+
     debug && console.log('Fetchier POST:', { json, body });
     return json;
   } catch(error){ throw error }
 }
 
 async function PUT({ url, body, method = 'PUT', jsonBody, headers = {}, debug}){
-  if(!url) 
+  if(!url)
     throw new Error('url is missing');
-  
+
   body = jsonBody ? JSON.stringify(body) : body;
-  
+
   try{
     const res = await fetch(url, { method, body, headers });
     // const json = await res.json();
     if(res && res.status !== 200)
       throw res;
-    
+
     debug && console.log('Fetchier PUT:', res);
     return res;
   } catch(error){ throw error }
-  
+
 }
 
 async function GQL({ query, GQ, url, token, variables, headers = {}, debug }){
   GQ = typeof ENV === 'object' && ENV.GQ || GQ;
-  
+
   url = url || GQL_URL + GQ;
-  
+
   const opts = {
     method: 'POST',
-    headers: { 
+    headers: {
       'content-type': 'application/json',
       ...(token && {'authorization':  `Bearer ${token}`} || {}),
       ...headers
     },
     body: JSON.stringify({ query, variables })
   }
-  
+
   const res = await fetch(url, opts);
   const json = await res.json();
-  
+
   debug && debug && console.log('Fetchier GQL:', json);
-  
+
   if(json.errors){
     const error = json.errors.shift();
     throw error.functionError || error.message || error;
   }
-  
+
   const keys = Object.keys(json);
   return keys.length && json[keys.shift()];
 }
 
-function wsGQL({ GQ, token, url, protocolOld, debug, onError = console.error }, cb) {
+function wsGQL({ GQ, token, url, protocolOld, debug, onError = console.error, headers = {} }, cb) {
   GQ = typeof ENV === 'object' && ENV.GQ || GQ;
-  
+
   if(webSockets[url]){
     console.log(`WebSocket ${url} is already open`);
     return Promise.resolve(webSockets[url]);
   }
-  
+
   url = url || WSS_URL + GQ;
-  
+
   webSockets[url] = new WebSocket(url, protocolOld ? WSS_PROTOCOL_OLD : WSS_PROTOCOL);
   webSocketSubscriptions[url] = {};
-  
+
   webSockets[url].onopen = e => {
     // send handshake
     webSockets[url].send(JSON.stringify({
       type: protocolOld ? 'init' : 'connection_init',
       payload: {
-        Authorization: `Bearer ${token}`,
+        ...(token && {'Authorization':  `Bearer ${token}`} || {}),
         headers: {
           'content-type': 'application/json',
-          authorization: `Bearer ${token}`
+          ...(token && {'authorization':  `Bearer ${token}`} || {}),
+          ...headers
         }
       }
     }))
   }
-  
+
   // webSocket.onclose = () => {
   //   isSocketConnected = false;
   //   webSocket.close() // disable onclose handler first
   // }
   return new Promise( (resolve, reject) => {
-  
+
     webSockets[url].onmessage = e => {
       const data = JSON.parse(e.data);
-      
+
       debug && console.log(data.type);
-      
+
       switch(data.type){
-        
+
         case 'init_success':
         case 'connection_ack':
           debug && console.log('Fetchier wsGQL:', url, 'socket connected');
@@ -176,24 +177,24 @@ function wsGQL({ GQ, token, url, protocolOld, debug, onError = console.error }, 
           return resolve(webSockets[url]);
           // return cb && cb(webSockets[url]);
         break;
-        
+
         case 'subscription_data':
         case 'data':
           const payload = data && data.payload && data.payload.data;
-          
-          if(!payload) 
+
+          if(!payload)
             return debug && console.warn('Fetchier wsGQL issue:', data);
-            
+
           debug && console.log('Fetchier wsGQL:', { payload }, data);
           const keys = typeof payload === 'object' ? Object.keys(payload) : [];
-          
+
           const action = webSocketSubscriptions[url][data.id];
           action && action(keys.length && payload[keys.shift()] || payload)
         break;
-        
+
         case 'error':
           onError(data)
-        
+
         case 'init_fail':
         case 'connection_error':
           return reject(data.payload);
@@ -203,34 +204,34 @@ function wsGQL({ GQ, token, url, protocolOld, debug, onError = console.error }, 
           // })
       }
     }
-    
+
   });
 }
 
 function wsGQLSubscribe({ url, subscription, debug }){
-  
+
   return subscribe(subscription);
-  
+
   function subscribe({id, query, action, variables}){
     if(!webSocketSubscriptions[url])
       return console.warn('Fetchier wsGQLSubscribe', `"${id}"`, 'can not subscribe without existing socket', url);
-    if(webSocketSubscriptions[url][id]) 
+    if(webSocketSubscriptions[url][id])
       return console.warn('Fetchier wsGQLSubscribe', `"${id}"`, 'subscription already exists');
-    
+
     webSocketSubscriptions[url][id] = action;
     const payload = { id: String(id), type: 'start', payload: { query, variables } };
     if(!webSockets[url]) return;
     webSockets[url].send(JSON.stringify(payload));
     return debug && console.log('Fetchier wsGQLSubscribe start', id, url, payload);
   }
-  
+
   // queries.forEach( (query, id) => {
   //   const payload = { id: String(id), type: 'start', payload: { query } };
   //   debug && console.log('Fetchier wsGQLSubscribe', { url, payload });
   //   if(!webSockets[url]) return;
   //   webSockets[url].send(JSON.stringify(payload));
   // });
-  
+
   return;
 }
 
@@ -244,20 +245,20 @@ function wsGQLUnsubscribe({ url, id, debug }){
 
 function wsGQLclose(props = {}){
   return new Promise(( resolve, reject ) => {
-    
+
     if(props.url && webSockets[props.url]){
       webSockets[props.url].close();
       delete webSockets[props.url];
       delete webSocketSubscriptions[props.url];
       return resolve();
     }
-    
+
     for( let url in webSockets ){
       webSockets[url].close();
       delete webSockets[url];
       delete webSocketSubscriptions[url];
     }
-    
+
     return resolve();
   })
 }
